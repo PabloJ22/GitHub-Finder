428dcb5850094a6607b79400a760d38337d987f2
APabloJ <altisimop@gmail.com>
2019-06-06 15:22:14 -0500

    Users component. changed class components that didnt require state in to func components.

--- github-finder/src/components/users/Users.js
+++ github-finder/src/components/users/Users.js
@@ -2,36 +2,11 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 class Users extends Component {
-
-  state = {
-    users: [
-      {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url:'https://github.com/mojombo'
-      },
-      {
-        id: 'id',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url:'https://github.com/defunkt'
-      },
-      {
-        id: 'id',
-        login: 'ezmobius',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/5?v=4',
-        html_url:'https://github.com/ezmobius'
-      }
-    ]
-  }
-
-
   render() {
     return (
       <div style={userStyle}>
 
-      {this.state.users.map(user =>(
+      {this.props.users.map(user =>(
         <UserItem key={user.id} user={user}/>
       ))}
         
