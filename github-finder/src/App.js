659908c3ff2bc5a54caddff93ca0db2448c457de
APabloJ <altisimop@gmail.com>
2019-06-05 21:33:46 -0500

    messing around with JSX.^~^

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,13 +1,29 @@
-import React, { Component} from 'react';
+import React, { Component, Fragment} from 'react';
 import './App.css';
 
 class App extends Component  {
 
+foo = () => 'Barz';
+
 render(){
+
+  const name = 'Pablito';
+  const loading = false;
+
+
+
+  const foo = () => 'foo mayne';
+
   return (
-    <div className="App">
-     <h1>Wassup from React</h1>
-    </div>
+    <Fragment>
+      {loading ? <h4>Loading</h4>: <h1>Wassup it's {this.foo()} from React</h1>}
+     <h1>Wassup it's {name} from React</h1>
+     <h1>Wassup it's {name.toUpperCase()} from React</h1>
+     <h1>Wassup it's {foo()} from React</h1>
+     <h1>Wassup it's {this.foo()} from React</h1>
+     <h1>{11+11}</h1>
+     <h1>{loading}</h1>
+    </Fragment>
   );
   
 }
