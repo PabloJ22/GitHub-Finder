0ea35bd52b036ed4a458e5d3c08d12c556177647
APabloJ <altisimop@gmail.com>
2019-06-06 10:29:48 -0500

    added css && created Navbar.js component.

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,28 +1,17 @@
 import React, { Component, Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 class App extends Component  {
 
-foo = () => 'Barz';
 
 render(){
 
-  const name = 'Pablito';
-  const loading = false;
-
-
-
-  const foo = () => 'foo mayne';
 
   return (
     <Fragment>
-      {loading ? <h4>Loading</h4>: <h1>Wassup it's {this.foo()} from React</h1>}
-     <h1>Wassup it's {name} from React</h1>
-     <h1>Wassup it's {name.toUpperCase()} from React</h1>
-     <h1>Wassup it's {foo()} from React</h1>
-     <h1>Wassup it's {this.foo()} from React</h1>
-     <h1>{11+11}</h1>
-     <h1>{loading}</h1>
+      <Navbar />
     </Fragment>
   );
   
