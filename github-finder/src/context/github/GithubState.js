1002da9ffd475fd04096d9b29302cd49b5a8ccd5
APabloJ <altisimop@gmail.com>
2019-07-13 19:38:10 -0500

    created an Alert State, Context, and Reducer. cleaned up App.js. now everything is more organized using the context api, useContext and useReducer Hook.

--- github-finder/src/context/github/GithubState.js
+++ github-finder/src/context/github/GithubState.js
@@ -8,7 +8,8 @@ GET_USER ,
 CLEAR_USERS, 
 GET_REPOS ,
 SET_LOADING,
-} from './types';
+} from '../github/types.js';
+
 
 const GithubState = props => {
   const initialState = {
@@ -35,24 +36,55 @@ dispatch({
 }
 
  // Get user
+ const getUser = async(username) => {
+  setLoading();
+
+  const res = await axios
+
+.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+dispatch({
+  type: GET_USER,
+  payload: res.data
+});
+     };
+
 
  // Get repos
 
+ const getUserRepos = async(username) => {
+  setLoading(true);
+ const res = await axios
+
+.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+dispatch({
+  type: GET_REPOS,
+  payload: res.data
+});   
+}
+
+
  //clear users
+ const clearUsers = () => dispatch({type:CLEAR_USERS});
 
  //set loading
- const setLoading = () => dispatch({type:SET_LOADING})
+ const setLoading = () => dispatch({type:SET_LOADING});
 
- return <GithubContext.Provider
+ return (<GithubContext.Provider
  value={{
    users: state.users,
    user: state.user,
    repos: state.repos,
    loading: state.loading,
-   searchUsers
+   searchUsers,
+   clearUsers,
+   getUser,
+   getUserRepos
  }}>
  {props.children}
  </GithubContext.Provider>
-} 
+ );
+}; 
 
 export default GithubState;
